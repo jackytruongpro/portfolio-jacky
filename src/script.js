d4ce5a52d8e6389302946e43c06476e32e1d0e96
jackytruongpro <jacky-t@hotmail.fr>
2023-06-21 16:10:48 +0200

    Add panel page

--- src/script.js
+++ src/script.js
@@ -1,32 +1,175 @@
 import * as THREE from "three"
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
+//---------
+// Scene
+//---------
 const scene = new THREE.Scene();
+const canvas = document.querySelector('.webgl');
+
 scene.background = new THREE.CubeTextureLoader()
-	.setPath( '../static/textures/roomCube/' )
-	.load( [
+	.setPath('/static/textures/roomCube/')
+	.load([
 		'px.png',
 		'nx.png',
 		'py.png',
 		'ny.png',
 		'pz.png',
 		'nz.png'
-	] );
+	]);
 
-// Sizes
 const sizes = {
-    width: 800, 
-    height: 600
+	width: window.innerWidth,
+	height: window.innerHeight
+	// width: 500,
+	// height: 500
+};
+
+//---------
+// Axe helper
+//---------
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
+
+//---------
+// Camera
+//---------
+const camera = new THREE.PerspectiveCamera(70, sizes.width / sizes.height);
+camera.position.z = 10;
+scene.add(camera);
+
+canvas.addEventListener('wheel', handleMouseWheel);
+
+// Fonction de gestion de l'événement de la molette de la souris
+function handleMouseWheel(event) {
+  event.preventDefault();
+
+  // Calcule le facteur de zoom en fonction de la direction de la molette de la souris
+  const zoomSpeed = 0.0001; // Ajuste la vitesse de zoom selon tes besoins
+  const zoomFactor = 1 - event.deltaY * zoomSpeed * 10;
+
+	// Définit les valeurs de zoom minimale et maximale
+	const minZoom = 1; // Valeur de zoom minimale
+  const maxZoom = 3.0; // Valeur de zoom maximale
+
+  // Applique le zoom à la caméra
+  camera.zoom *= zoomFactor;
+	camera.zoom = Math.max(minZoom, Math.min(maxZoom, camera.zoom));
+  camera.updateProjectionMatrix();
 }
 
-// Camera 
-const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height); 
-camera.position.z = 10
-scene.add( camera );
 
+//---------
 // Renderer
-const canvas = document.querySelector(".webgl");
+//---------
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
 
-const renderer = new THREE.WebGLRenderer({
-    canvas
+
+//---------
+// Controls
+//---------
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.maxPolarAngle = (Math.PI / 2) + 0.1
+
+const tick = () => {
+	controls.update();
+	renderer.render(scene, camera);
+	window.requestAnimationFrame(tick);
+}
+tick()
+
+
+//---------
+// Objects
+//---------
+
+// Poster
+const posters = [];
+
+function add_poster(poster_id, poster_name, poster_width, poster_height, poster_position_x, poster_position_y, poster_position_z) {
+	const geometry = new THREE.PlaneGeometry( poster_width, poster_height );
+	const material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+	const poster = new THREE.Mesh( geometry, material );
+	poster.userData.id = poster_id;
+	poster.name = poster_name;
+	poster.position.x = poster_position_x;
+	poster.position.y = poster_position_y;
+	poster.position.z = poster_position_z;
+	// poster.rotation.y = -150;
+	posters.push(poster);
+	scene.add(poster);
+}
+
+add_poster(1,'Poster 1', 200, 261, -500, 0, -1000);
+add_poster(2,'Poster 2', 200, 261, 0, 0, -1000);
+add_poster(3,'Poster 3', 200, 261, 500, 0, -1000);
+
+
+//----------
+// Raycaster
+//----------
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+// Eventlistener (onClick)
+canvas.addEventListener('click', onClick);
+
+function onClick(event) {
+    const rect = canvas.getBoundingClientRect();
+    const x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+    const y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
+    mouse.x = x;
+    mouse.y = y;
+
+    raycaster.setFromCamera(mouse, camera);
+
+    const intersects = raycaster.intersectObjects(posters, true);
+
+    if (intersects.length > 0) {
+        // Au click
+        const object = intersects[0].object;
+			
+				const objectId = document.getElementById(object.userData.id);
+
+				toggleHidden(objectId)
+
+        console.log(`Forme géométrique touchée : ${object.userData.id}`);
+    }
+}
+
+closeButton.addEventListener('click', function(){
+	console.log(closeButton.parent.nodeName);
 })
-renderer.setSize( sizes.width, sizes.height );
+
+function toggleHidden(element){
+	if (element.classList.contains('hidden')) {
+		element.classList.remove('hidden');
+		// element.classList.add('visible');
+	} else {
+		// element.classList.remove('visible');
+		element.classList.add('hidden');
+	}
+}
+
+
+// canvas.addEventListener('mouseover', onMouseMove);
+
+// function onMouseMove(event) {
+//   const rect = canvas.getBoundingClientRect();
+//   const x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+//   const y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
+//   mouse.x = x;
+//   mouse.y = y;
+
+//   raycaster.setFromCamera(mouse, camera);
+
+//   const intersects = raycaster.intersectObjects(posters, true);
+
+//   if (intersects.length > 0) {
+//     const object = intersects[0].object;
+//     alert(`Forme géométrique survolée : ${object.name}`);
+//   }
+// }
---
+++ src/technoScene.js
@@ -0,0 +1,178 @@
+import * as THREE from "three"
+
+//---------
+// Scene
+//---------
+const technoScene = new THREE.Scene(); 
+
+
+//---------
+// Axe helper
+//---------
+const axesHelper = new THREE.AxesHelper( 5 );
+technoScene.add( axesHelper );
+
+
+//---------
+// Light
+//---------
+const spotLight = new THREE.SpotLight( 0xffffff );
+spotLight.position.set( 0, 0, 500 );
+// spotLight.castShadow = true;
+technoScene.add( spotLight );
+
+
+//---------
+// Sizes
+//---------
+const sizes = {
+    width: 1400, 
+    height: 450
+}
+
+
+//---------
+// Camera
+//---------
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height); 
+camera.position.z = 10
+technoScene.add( camera );
+
+
+//---------
+// Renderer
+//---------
+const canvas = document.querySelector(".techno");
+
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+    alpha: true
+})
+renderer.setSize( sizes.width, sizes.height );
+renderer.setClearColor( 0x000000, 0 ); 
+
+
+//---------
+// Animate
+//---------
+const animate = () => {
+    renderer.render( technoScene, camera );
+    window.requestAnimationFrame( animate );
+}
+
+animate()
+
+
+//---------
+// Technos
+//---------
+const technos = [];
+
+function add_techno(techno_position_x, techno_name, techno_link, techno_image) {
+  const textureLoader = new THREE.TextureLoader();
+  const textureTechno = textureLoader.load(techno_image);
+  
+  const techno = new THREE.Mesh(
+    new THREE.BoxGeometry(1.5, 1.5, 1.5),
+    new THREE.MeshStandardMaterial({
+      map: textureTechno,
+      face: THREE.DoubleSide
+    })
+  )
+  techno.position.x = techno_position_x;
+  techno.position.y = 0;
+  techno.position.z = 0;
+  techno.name = techno_name;
+  techno.userData.link = techno_link;
+  techno.isDraggable = true;
+  technos.push(techno)
+  technoScene.add(techno);
+
+  const animate = () => {
+    techno.rotation.y += 0.01
+    window.requestAnimationFrame( animate );
+  }
+
+  animate();
+
+  let isMouseDown = false;
+  let selectedTechno = null;
+  let rotationSpeed = 0.005;
+
+  document.addEventListener('mousedown', onMouseDown, false);
+  document.addEventListener('mouseup', onMouseUp, false);
+  document.addEventListener('mousemove', onMouseMove, false);
+
+  let previousMousePosition = {
+    x: 0,
+    y: 0
+  };
+
+  function onMouseDown(event) {
+    isMouseDown = true;
+  
+    const intersects = raycaster.intersectObjects(technos, true);
+  
+    if (intersects.length > 0) {
+      selectedTechno = intersects[0].object;
+    }
+  }
+  
+  function onMouseUp() {
+    isMouseDown = false;
+    selectedTechno = null;
+  }
+  
+  function onMouseMove(event) {
+    if (isMouseDown && selectedTechno) {
+      const currentMousePosition = {
+        x: event.clientX,
+        y: event.clientY
+      };
+  
+      const deltaMove = {
+        x: currentMousePosition.x - previousMousePosition.x,
+        y: currentMousePosition.y - previousMousePosition.y
+      };
+  
+      selectedTechno.rotation.y += deltaMove.x * rotationSpeed;
+      selectedTechno.rotation.x += deltaMove.y * rotationSpeed;
+  
+      previousMousePosition = currentMousePosition;
+    }
+  }
+  
+
+}
+
+add_techno(-6,'Flutter', 'https://tickets-siae.com/fr/', '/static/image/techno/logo-flutter.png');
+add_techno(-3,'Dart', 'https://tickets-siae.com/fr/', '/static/image/techno/logo-dart.png');
+add_techno(0,'Node', 'https://tickets-siae.com/en/', '/static/image/techno/logo-node.png');
+add_techno(3,'Firebase', 'https://tickets-siae.com/fr/', '/static/image/techno/logo-firebase.png');
+add_techno(6,'Flutterflow', 'https://tickets-siae.com/fr/', '/static/image/techno/logo-flutterflow.png');
+
+//----------
+// On techno click
+//----------
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+canvas.addEventListener('click', onClick);
+
+function onClick(event) {
+    const rect = canvas.getBoundingClientRect();
+    const x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+    const y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
+    mouse.x = x;
+    mouse.y = y;
+
+    raycaster.setFromCamera(mouse, camera);
+
+    const intersects = raycaster.intersectObjects(technos, true);
+
+    if (intersects.length > 0) {
+        const object = intersects[0].object;
+				const objectId = document.getElementById(object.userData.link);
+        // window.open(object.userData.link);
+    }
+}
